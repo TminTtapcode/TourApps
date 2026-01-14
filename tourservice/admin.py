4c0f650bbc8cc723f87333d8b8c7ffbf987363d6
TminTtapcode <oakceniki@gmail.com>
2026-01-14 20:09:23 +0700

    Delete tourservice directory

--- tourservice/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Service, UserProfile
-
-admin.site.register(Service)
-admin.site.register(UserProfile)
--- tourservice/apps.py
+++
@@ -1,9 +0,0 @@
-# tourservice/apps.py
-from django.apps import AppConfig
-
-class TourserviceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tourservice'
-
-    def ready(self):
-        import tourservice.signals
